0fbd851be8acaeddb12c01179b0cb29cac890b52
LucasCancio <lucas.cancio7@gmail.com>
2020-06-24 01:07:58 -0300

    Implementando tela de GameOver

---
+++ scripts/jogo/imagem.js
@@ -0,0 +1,11 @@
+class Imagem {
+  constructor(imagem, largura, altura) {
+    this.imagem = loadImage(imagem);
+    this.largura = largura;
+    this.altura = altura;
+  }
+
+  exibe(x, y) {
+    image(this.imagem, x, y, this.largura, this.altura);
+  }
+}
--- sketch.js
+++ sketch.js
@@ -1,8 +1,10 @@
 let imagemCenario;
 let imagemPersonagem;
 let imagemInimigo;
+let imagemGameOver;
 
 let somDoPulo;
+let gameOverSom;
 
 let cenario;
 let somDoJogo;
@@ -11,22 +13,32 @@ let inimigo;
 
 const fps = 40;
 
+let larguraCentro;
+let alturaCentro;
+
 function preload() {
   imagemCenario = loadImage("./imagens/cenario/floresta.png");
   imagemPersonagem = loadImage("./imagens/personagem/correndo.png");
   imagemInimigo = loadImage("./imagens/inimigos/gotinha.png");
   somDoJogo = loadSound("./sons/trilha_jogo.mp3");
+  imagemGameOver = new Imagem("./imagens/assets/game-over.png",412, 78);
+
+  gameOverSom = loadSound("./sons/gameover-sound.mp3");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   cenario = new Cenario(imagemCenario, 3);
+  gameOverSom.setVolume(0.1);
   somDoJogo.setVolume(0.1);
   somDoJogo.loop();
   personagem = new Personagem(4, 4, imagemPersonagem, 0, 110, 135, 220, 270);
 
   inimigo = new Inimigo(4, 7, imagemInimigo, width - 52, 52, 52, 104, 104);
   frameRate(fps);
+
+  larguraCentro = width / 2;
+  alturaCentro = height / 2;
 }
 
 function keyPressed() {
@@ -47,13 +59,18 @@ function draw() {
   inimigo.exibe();
   inimigo.move();
 
+  
+
   if (personagem.estaColidindo(inimigo)) {
     finalizarJogo();
   }
 }
 
 function finalizarJogo() {
-  console.log("Colidiu");
-  noLoop();
+  imagemGameOver.exibe(larguraCentro - (imagemGameOver.largura/2), alturaCentro - (imagemGameOver.altura/2));
+
+  gameOverSom.play();
   somDoJogo.stop();
+
+  noLoop();
 }
